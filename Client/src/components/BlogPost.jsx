23ba2f223b2d3f43bf7eb28b8682320ff78d2aa2
Rashmi Verma <vermarashmi028@gmail.com>
2024-07-20 22:26:00 +0530

    commit

--- Client/src/components/BlogPost.jsx
+++ Client/src/components/BlogPost.jsx
@@ -6,7 +6,7 @@ import React from 'react';
     return (
         <a className="booxx" href={href}>
             <div style={{ width: '100%', height: '40vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <div style={{ width: '100%', height: '100%', borderRadius: '10px' }}>
+                <div style={{ width: '100%',marginRight:'15px', height: '100%', borderRadius: '10px' }}>
                     <img src={imgSrc} alt="pic of blogs" style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: '1rem' }} />
                 </div>
             </div>
@@ -17,4 +17,5 @@ import React from 'react';
         </a>
     );
 };
+
 
