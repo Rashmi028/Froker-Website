3904c304e0f2f0034ba89ddc976bb6e880860c6a
Rashmi Verma <vermarashmi028@gmail.com>
2024-07-20 16:34:20 +0530

    commit

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Newsletter from './components/Newsletter'
 import Body1 from './components/Body1'
+import Body2 from './components/Body2'
 function App() {
 
   return (
@@ -11,6 +12,7 @@ function App() {
     <Header/>  
     <div className='body'>
       <Body1/>
+      <Body2/>
       <Newsletter/>
     </div>
     <Footer/>
